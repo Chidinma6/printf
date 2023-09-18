0105bd92f8c7dc72667832e4cc3f9456882254ca
ekachnwa <onyekachi.nwaogwu@gmail.com>
2023-09-18 13:41:06 -0700

    remodification of functions on main.h

--- main.h
+++ main.h
@@ -51,7 +51,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /* -------------- FUNCTIONS CALLS --------------------- */
 
-/* Funtions to print chars and strings */
+/* The Funtions prints chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -59,7 +59,7 @@ int print_string(va_list types, char buffer[],
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Functions to print numbers */
+/* The Functions prints numbers */
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
@@ -76,11 +76,11 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non printable characters */
+/* The Function prints non printable characters */
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* The Function prints memory address */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
