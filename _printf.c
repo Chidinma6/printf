0e17eea368ac83efbabadad04251d4453fe17b2c
Chidinma6 <chidinmaekuma420@gmail.com>
2023-09-18 09:30:10 -0700

    commit

---
+++ _printf.c
@@ -0,0 +1,75 @@
+#include "main.h"
+
+int _printf(const char *format, ...);
+
+int main(){
+    _printf("Chidinma\n");
+    _printf("This is a letter of an alphabet: %c\n", 'w');
+    _printf("A doctor works at a: %s\n", "hospital");
+    _printf("Double percent sign: %%\n");
+
+    return 0;
+}
+
+
+
+
+
+int _printf(const char *format, ...){
+
+    int chara_print = 0;
+    va_list list_of_args;
+
+    // if (format == NULL){
+    //     return (-1);
+    // }
+
+    va_start(list_of_args, format);
+
+    while (*format) // iterates through all the characters in the format string
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            chara_print++;
+        }
+        else{ //if format is % sign
+            format++; //skips % amd then checks the next character
+        
+            if (*format == '\0'){
+                break;
+            }
+
+            if (*format == '%')
+            { //handles double % "%%"
+                write(1, format, 1);
+                chara_print++;
+            }
+
+            else if (*format == 'c'){
+                char c = va_arg(list_of_args, int);
+                write(1, &c, 1);
+                chara_print++;
+            }
+
+            else if(*format == 's'){
+                char *str = va_arg(list_of_args, char*);
+                int str_len = 0;
+
+                //calculate the string length
+                while (str[str_len] != '\0'){
+                    str_len++;
+                }
+
+                write(1, str, str_len);
+                chara_print += str_len;
+            } 
+        }
+
+        format++;
+    }
+
+    va_end(list_of_args);
+    
+    return chara_print;
+}
