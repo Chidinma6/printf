902fd3ad17ed4331682effced7f4dec8886c54f3
ekachnwa <onyekachi.nwaogwu@gmail.com>
2023-09-19 23:21:37 -0700

    util and handlers added to main.h file

--- main.h
+++ main.h
@@ -84,16 +84,19 @@ int print_rot13string(va_list types, char buffer[],
 
 /* Handler */
 int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[],
-	int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-	int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start);
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
 
+/* ---------- UTIL ---------- */
+int is_printable(char);
+long int convert_size_unsgnd(unsigned long int num, int size);
+int is_digit(char);
+int append_hexa_code(char, char[], int);
+long int convert_size_number(long int num, int size);
 
+#endif
 
 
 /*
@@ -101,7 +104,3 @@ int _printf(const char *format, ...);
 int binary (int num);
 int octal(int num);
 */
-
-
-
-#endif
